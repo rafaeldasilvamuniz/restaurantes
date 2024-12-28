c6a6fb2873faf4c7a2a90b238302bb4677814ff7
rafaeldasilvamuniz <86721430+rafaeldasilvamuniz@users.noreply.github.com>
2024-12-27 21:15:33 -0300

    Delete src directory

--- src/components/Banner/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Imagem } from './styles'
-
-import bannerImg from '../../assets/images/apresentacao.png'
-
-const Banner = () => (
-  <Imagem style={{ backgroundImage: `url(${bannerImg})` }}></Imagem>
-)
-
-export default Banner
